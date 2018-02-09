b33f96b27a3c252031e3755accb0b1868a4738ef
ebinezargnan <gnanasekaran.e@gmail.com>
2018-02-09 21:00:57 +0530

    Adding ArrayList

--- BusinessObjects/com/cosmo/HelloWorld.java
+++ BusinessObjects/com/cosmo/HelloWorld.java
@@ -1,8 +1,10 @@
 package com.cosmo;
+import java.util.*;
 public class HelloWorld{
  public static void main(String ar[]){
 
   System.out.println("Welcome");
-
+  List<String> newList=new ArrayList<String>();
+  
 }
 }
