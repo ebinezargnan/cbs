56f16d8b314deed7b3d5fe4d0d2f71155845f932
ebinezargnan <gnanasekaran.e@gmail.com>
2018-02-12 15:28:22 +0530

    Addding the new file WelcomeWorld.java

---
+++ BusinessObjects/com/cosmo/WelcomeWorld.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class WelcomeWorld{
+ public static void main(String ar[]){
+
+  System.out.println("Welcome");
+  List<String> newList=new ArrayList<String>();
+  
+}
+}
